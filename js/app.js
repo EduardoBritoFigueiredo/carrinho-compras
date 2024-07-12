51f748a6092c864652ac6ef116228d9325e07ad5
eduardo.brito <eduardo.brito@plannersistemas.com.br>
2024-07-12 20:10:41 -0300

    feat: create atualizarTotal and updates

--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
+let carrinho = document.querySelector('.carrinho__produtos__produto');
 let quantidade;
-function adicionar() {
-  quantidade = document.getElementById('quantidade').value;
-  let produtoComPreco = document.getElementById('produto').value.split(' - R$');
+let total = 0;
 
-  colocarNoCarrinho(quantidade, produtoComPreco[0], produtoComPreco[1]);
+function adicionar() {
+  let elementoQuantidade = document.getElementById('quantidade');
+  quantidade = elementoQuantidade.value;
+  let produtoComPreco = document.getElementById('produto').value.split(' - R$');
+  let produto = produtoComPreco[0];
+  let preco = produtoComPreco[1];
+  total += (preco * quantidade);
+
+  colocarNoCarrinho(quantidade, produto, preco);
+  atualizarTotal();
+  elementoQuantidade.value = '';
 }
 
 function colocarNoCarrinho(quantidade, produto, preco) {
-  let carrinho = document.querySelector('.carrinho__produtos__produto');
-  let novoItem = `<br><span class="texto-azul">${quantidade}x</span>${produto}<span class="texto-azul">R$${preco}</span>`;
+  let novoItem = `<br><span class="texto-azul">${quantidade}x </span>${produto} - <span class="texto-azul">R$${preco}</span>`;
 
   carrinho.innerHTML += novoItem;
+}
+
+function atualizarTotal() {
+  console.log(`total = ${total}`)
+  let elementoTotal = document.getElementById('valor-total');
+  console.log(`elementoTotal = ${elementoTotal.innerText}`);
+  elementoTotal.innerText = `R$${total}`;
+}
+
+function limpar() {
+  carrinho.innerHTML = '';
 }
